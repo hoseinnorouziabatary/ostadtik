e57f57fd7452ff1107c7fa6319c23bf62b38e22a
hosein <40340902+hoseinnorouziabatary@users.noreply.github.com>
2018-07-22 12:54:13 +0430

    Update startup.py

--- startup.py
+++ startup.py
@@ -1,10 +1,17 @@
 
-from ostadtik import app
+
+from ostadtik import Class, comment, Calltime, Course, price, Profile, Student, Teacher
 
 __author__ = "NOROUZI"
 
-app.run(Debug=True)
-
+Teacher.app.run(Debog=True)
+Student.app.run(Debog=True)
+Class.app.run(Debog=True)
+comment.app.run(Debog=True)
+Calltime.app.run(Debog=True)
+Course.app.run(Debog=True)
+price.app.run(Debog=True)
+Profile.app.run(Debog=True)
 
 
 
