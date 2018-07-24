4bb8431dca4c08467adc2596b8d39cc62f467a50
hosein <40340902+hoseinnorouziabatary@users.noreply.github.com>
2018-07-24 08:19:14 +0430

    Update __init__.py

--- ostadtik/Class/__init__.py
+++ ostadtik/Class/__init__.py
@@ -1,34 +1,26 @@
-from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from ostadtik.Route import clss
+
 
 __author__ = "NOROUZI"
 
-app = Flask(__name__)
-app.register_blueprint(clss)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://scott:root@localhost/OSTADTIK'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = b'\xaelX\x1fI\xa4\xfc{\xb8\x11Zk\xa1\xdb,\xf7L5[Bty\x1c\xb4'
-db = SQLAlchemy(app)
+dcl = SQLAlchemy()
 
 
-class Classtable(db.Model):
+class Classtable(dcl.Model):
     __table_name__ = "ClassTable"
-    classtableid = db.Column(db.NVARCHAR(50), primary_key=True, unique=True, nullable=True)
-    price = db.Column(db.INT, db.ForeignKey('Course.Price'), nullable=True)
-    hoursone = db.Column(db.NVARCHAR(40), db.ForeignKey('Course.HoursOne'), nullable=True)
-    number_count = db.Column(db.NVARCHAR(40), db.ForeignKey('Course.NumberCountClass'), nullable=True)
-    firstnamestudent = db.Column(db.NVARCHAR(40), db.ForeignKey('Student.FirstNameStudent'), nullable=True)
-    firstnameteacher = db.Column(db.NVARCHAR(40), db.ForeignKey('Teacher.FirstNameTeacher'), nullable=True)
-    lastnamestudent = db.Column(db.NVARCHAR(40), db.ForeignKey('Student.LastNameStudent'), nullable=True)
-    lastnameteacher = db.Column(db.NVARCHAR(40), db.ForeignKey('Teacher.LastNameTeacher'), nullable=True)
-    teacherid = db.Column(db.NVARCHAR(50), db.ForeignKey('Teacher.TeacherID'), nullable=True)
-    studentid = db.Column(db.NVARCHAR(50), db.ForeignKey('Student.StudentID'), nullable=True)
-    courseid = db.Column(db.NVARCHAR(50), db.ForeignKey('Course.CourseID'), nullable=True)
-    day = db.Column(db.NVARCHAR(40), db.ForeignKey('Course.Day'), nullable=True)
-    hoursinday = db.Column(db.NVARCHAR(40), db.ForeignKey('Course.HoursInDay'), nullable=True)
-    temp1 = db.Column(db.BOOLEAN, nullable=True, temp1=False)
-    temp2 = db.Column(db.BOOLEAN, nullable=True, temp2=False)
+    classtableid = dcl.Column(dcl.NVARCHAR(50), primary_key=True, unique=True, nullable=True)
+    price = dcl.Column(dcl.INT, dcl.ForeignKey('course.Price'), nullable=True)
+    hoursone = dcl.Column(dcl.NVARCHAR(40), dcl.ForeignKey('course.HoursOne'), nullable=True)
+    number_count = dcl.Column(dcl.NVARCHAR(40), dcl.ForeignKey('course.NumberCountClass'), nullable=True)
+    firstnamestudent = dcl.Column(dcl.NVARCHAR(40), dcl.ForeignKey('student.FirstNameStudent'), nullable=True)
+    firstnameteacher = dcl.Column(dcl.NVARCHAR(40), dcl.ForeignKey('teacher.FirstNameTeacher'), nullable=True)
+    lastnamestudent = dcl.Column(dcl.NVARCHAR(40), dcl.ForeignKey('student.LastNameStudent'), nullable=True)
+    lastnameteacher = dcl.Column(dcl.NVARCHAR(40), dcl.ForeignKey('teacher.LastNameTeacher'), nullable=True)
+    teacherid = dcl.Column(dcl.NVARCHAR(50), dcl.ForeignKey('teacher.TeacherID'), nullable=True)
+    studentid = dcl.Column(dcl.NVARCHAR(50), dcl.ForeignKey('student.StudentID'), nullable=True)
+    courseid = dcl.Column(dcl.NVARCHAR(50), dcl.ForeignKey('course.CourseID'), nullable=True)
+    day = dcl.Column(dcl.NVARCHAR(40), dcl.ForeignKey('course.Day'), nullable=True)
+    hoursinday = dcl.Column(dcl.NVARCHAR(40), dcl.ForeignKey('course.HoursInDay'), nullable=True)
 
     def __init__(self, price, hoursone, number_count, firstnamestudent, firstnameteacher, lastnamestudent,
                  lastnameteacher, day, hoursinday):
