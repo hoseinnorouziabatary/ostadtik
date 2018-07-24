c0a148a9633857a50f660e6baf0c98a4160edece
hosein <40340902+hoseinnorouziabatary@users.noreply.github.com>
2018-07-24 09:41:41 +0430

    Update __init__.py

--- ostadtik/Course/__init__.py
+++ ostadtik/Course/__init__.py
@@ -1,39 +1,29 @@
 
 
-from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_marshmallow import Marshmallow
-from ostadtik.Route import cour
 
 
 __author__ = "NOROUZI"
 
-
-app = Flask(__name__)
-app.register_blueprint(cour)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://scott:root@localhost/OSTADTIK'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = b'\xaelX\x1fI\xa4\xfc{\xb8\x11Zk\xa1\xdb,\xf7L5[Bty\x1c\xb4'
-db = SQLAlchemy(app)
-ma = Marshmallow(app)
+dc = SQLAlchemy()
 
 
-class Course(db.Model):
+class Course(dc.Model):
     __table_name__ = "Course"
-    courseid = db.Column(db.NVARCHAR(50), primary_key=True, unique=True, nullable=True)
-    area = db.Column(db.NVARCHAR(40), nullable=True)
-    city = db.Column(db.NVARCHAR(40), nullable=True)
-    secctionname = db.Column(db.NVARCHAR(40), nullable=True)
-    hoursone = db.Column(db.NVARCHAR(40), nullable=True)
-    number_count = db.Column(db.NVARCHAR(40), nullable=True)
-    price = db.Column(db.INT, nullable=True)
-    lastname = db.Column(db.NVARCHAR(40), nullable=True)
-    firstname = db.Column(db.NVARCHAR(40), nullable=True)
-    count_class = db.Column(db.NVARCHAR(40), nullable=True)
-    capacity = db.Column(db.NVARCHAR(40), nullable=True)
-    day = db.Column(db.NVARCHAR(40), nullable=True)
-    hoursinday = db.Column(db.NVARCHAR(40), nullable=True)
-    teacherid = db.Column(db.NVARCHAR(50), db.ForeignKey('Teacher.TeacherID'), nullable=True)
+    courseid = dc.Column(dc.NVARCHAR(50), primary_key=True, unique=True, nullable=True)
+    area = dc.Column(dc.NVARCHAR(40), nullable=True)
+    city = dc.Column(dc.NVARCHAR(40), nullable=True)
+    secctionname = dc.Column(dc.NVARCHAR(40), nullable=True)
+    hoursone = dc.Column(dc.NVARCHAR(40), nullable=True)
+    number_count = dc.Column(dc.NVARCHAR(40), nullable=True)
+    price = dc.Column(dc.INT, nullable=True)
+    lastname = dc.Column(dc.NVARCHAR(40), nullable=True)
+    firstname = dc.Column(dc.NVARCHAR(40), nullable=True)
+    count_class = dc.Column(dc.NVARCHAR(40), nullable=True)
+    capacity = dc.Column(dc.NVARCHAR(40), nullable=True)
+    day = dc.Column(dc.NVARCHAR(40), nullable=True)
+    hoursinday = dc.Column(dc.NVARCHAR(40), nullable=True)
+    teacherid = dc.Column(dc.NVARCHAR(50), dc.ForeignKey('teacher.TeacherID'), nullable=True)
 
     def __init__(self, area, city, secctionname, hoursone, number_count, price, lastname, firstname, count_class,
                  capacity, day, hoursinday):
@@ -51,4 +41,5 @@ class Course(db.Model):
         self.day = hoursinday
 
     def __repr__(self):
-        return '<Course SecctionName{},City{},Area{} >' .format(self.secctionname, self.city, self.area)
+        return '<' \
+               'Course SecctionName{},City{},Area{} >' .format(self.secctionname, self.city, self.area)
