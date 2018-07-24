a728a9ad757c775be9900661354e5445f597f8e6
hosein <40340902+hoseinnorouziabatary@users.noreply.github.com>
2018-07-24 09:46:42 +0430

    Update __init__.py

--- ostadtik/Teacher/__init__.py
+++ ostadtik/Teacher/__init__.py
@@ -1,31 +1,23 @@
 
 
-from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin, login_manager
-from ostadtik.Route import Tech
+
 
 __author__ = "NOROUZI"
-
-app = Flask(__name__)
-app.register_blueprint(Tech)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://scott:root@localhost/OSTADTIK'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = b'\xaelX\x1fI\xa4\xfc{\xb8\x11Zk\xa1\xdb,\xf7L5[Bty\x1c\xb4'
-db = SQLAlchemy(app)
-
+dt = SQLAlchemy()
 Login_manager = login_manager
 Login_manager.session_protection = "strong"
 
 
-class Teacher (db.Model, UserMixin):
-    __table_name__ = "Teacher"
-    teacherid = db.Column(db.NVARCHAR(50), unique=True, primary_key=True, nullable=True)
-    educationalrecords = db.Column(db.NVARCHAR(40), nullable=True)
-    academichonors = db.Column(db.NVARCHAR(40), nullable=True)
-    lastnameteacher = db.Column(db.NVARCHAR(40), nullable=True)
-    firstnameteacher = db.Column(db.NVARCHAR(40), nullable=True)
-    studentid = db.Column(db.NVARCHAR(40), db.ForeignKey('Student.StudentID'), nullable=True)
+class Teacher (dt.Model, UserMixin):
+    __table_name__ = "teacher"
+    teacherid = dt.Column(dt.NVARCHAR(50), unique=True, primary_key=True, nullable=True)
+    educationalrecords = dt.Column(dt.NVARCHAR(40), nullable=True)
+    academichonors = dt.Column(dt.NVARCHAR(40), nullable=True)
+    lastnameteacher = dt.Column(dt.NVARCHAR(40), nullable=True)
+    firstnameteacher = dt.Column(dt.NVARCHAR(40), nullable=True)
+    studentid = dt.Column(dt.NVARCHAR(40), dt.ForeignKey('student.StudentID'), nullable=True)
 
     def __init__(self, educationalrecords, academichonors, lastnameteacher, firstnameteacher):
         self.educationalrecords = educationalrecords
@@ -34,4 +26,4 @@ class Teacher (db.Model, UserMixin):
         self.firstnameteacher = firstnameteacher
 
     def __repr__(self):
-        return '<Teacher LastNameTeacher{} >' .format(self.LastNameTeacher)
+        return '<teacher LastNameTeacher{} >' .format(self.LastNameTeacher)
