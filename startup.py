d0026bfccfe9effbec6c6d9118ff12ccfc55b66d
Hosein Norouzi <hosein.norouzi1990@gmail.com>
2018-07-27 00:21:46 +0430

    updateostadbank

--- startup.py
+++ startup.py
@@ -10,17 +10,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:norouziabatary@localhost/O
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = b'\xaelX\x1fI\xa4\xfc{\xb8\x11Zk\xa1\xdb,\xf7L5[Bty\x1c\xb4'
 
-# app.register_blueprint(cal)
-# app.register_blueprint(clss)
-# app.register_blueprint(cmt)
-# app.register_blueprint(cour)
-# app.register_blueprint(prc)
-# app.register_blueprint(pro)
-# app.register_blueprint(stu)
-# app.register_blueprint(Tech)
-
 app = create_app(app)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True, port=3000)
-
