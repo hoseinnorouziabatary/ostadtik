b913e5acb322267bfa8d30e3f27fcfecb7894232
Hosein Norouzi <hosein.norouzi1990@gmail.com>
2018-07-26 12:56:41 +0430

    update teacher

---
+++ ostadtik/teacher/api.py
@@ -0,0 +1,27 @@
+from flask import Blueprint, request, flash, jsonify
+
+from ostadtik.route import db
+from ostadtik.teacher import Teacher
+from ostadtik.Student import Student
+
+
+__author__ = "NOROUZI"
+
+teacher = Blueprint("teacher", __name__)
+
+
+@teacher.route('/register_teacher', methods=["GET", "POST"])
+def register_teacher():
+    educationalrecords = request.json['']
+    academichonors = request.json['']
+    lastnameteacher = request.json['']
+    firstnameteacher = request.json['']
+
+    Student.i.query.filter_by(Student.flag).Update(flag=True)
+
+    db.session.add(Teacher(educationalrecords=educationalrecords, academichonors=academichonors,
+                           lastnameteacher=lastnameteacher, firstnameteacher=firstnameteacher, ))
+    db.session.commit()
+
+    flash("User registered successfully !")
+    return jsonify('hello')
