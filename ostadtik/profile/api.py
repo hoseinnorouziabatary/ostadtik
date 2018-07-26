ff389240c87c82f4676ef654f6718ebef95e9f62
Hosein Norouzi <hosein.norouzi1990@gmail.com>
2018-07-26 12:54:33 +0430

    updating

---
+++ ostadtik/profile/api.py
@@ -0,0 +1,21 @@
+from flask import Blueprint, request
+
+from ostadtik.route import db
+from ostadtik.profile import Profile
+
+__author__ = "NOROUZI"
+
+profile = Blueprint("profile", __name__)
+
+
+@profile.route('/register_profile', methods=["POST"])
+def registerprofile():
+    pdf = request.json['']
+    video = request.json['']
+    pictuer = request.json['']
+    lastname = request.json['']
+    firstname = request.json['']
+    activenon = request.json['']
+
+    db.session.add(Profile(pdf=pdf, video=video, pictuer=pictuer, lastname=lastname, firstname=firstname,
+                           activenon=activenon))
