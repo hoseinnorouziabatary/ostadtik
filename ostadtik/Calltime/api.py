8bcf08aee315eafd7b81b00f7d12c68bed4c47ee
Hosein Norouzi <hosein.norouzi1990@gmail.com>
2018-07-28 09:16:16 +0430

    updatejson

--- ostadtik/Calltime/api.py
+++ ostadtik/Calltime/api.py
@@ -10,16 +10,16 @@ calltime = Blueprint("calltime", __name__)
 
 @calltime.route('/call', methods=['POST'])
 def cal():
-    azinhour = request.json['']
-    tainhour = request.json['']
-    azinminutes = request.json['']
-    tainminutes = request.json['']
-    azinyy = request.json['']
-    tainyy = request.json['']
-    azinmm = request.json['']
-    tainmm = request.json['']
-    azindd = request.json['']
-    taindd = request.json['']
+    azinhour = request.json['StartHour']
+    tainhour = request.json['EndHour']
+    azinminutes = request.json['StartMinutes']
+    tainminutes = request.json['EndMinutes']
+    azinyy = request.json['StartYear']
+    tainyy = request.json['EndYear']
+    azinmm = request.json['StartMonth']
+    tainmm = request.json['EndMonth']
+    azindd = request.json['StartDay']
+    taindd = request.json['EndDay']
 
     db.session.add(Call(azinhour=azinhour, tainhour=tainhour, azinminutes=azinminutes, tainminutes=tainminutes,
                         azinyy=azinyy, tainyy=tainyy, azinmm=azinmm, tainmm=tainmm, azindd=azindd, taindd=taindd))
